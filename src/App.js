0bbcbcea2c5794cb73f1311279d18be137185c50
ADNguyen <nguyenad92@gmail.com>
2019-10-13 20:54:51 -0700

    Add auth and API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
+
 function App() {
   return (
     <div className="App">
@@ -23,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App, {includeGreetings: true});
